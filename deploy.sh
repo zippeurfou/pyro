da26332aeb4a6c659ef728f2984cbed9934c12ec
jpchen <jonathanp.chen@gmail.com>
2017-11-02 21:30:52 -0700

    update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,14 @@
 # clean old static files 
-echo "clean old static directorys? [n]: "
+echo "changing branches to master"
+# CHANGE THIS TO MASTER
+git checkout dev 
+echo "building docs"
+make docs
+echo "moving docs to tmpdocs"
+mv docs/source tmpdocs 
+echo "changing branch to gh-pages"
+git checkout gh-pages
+echo -n "clean old static directorys? [n]: "
 read x 
 if [ "$x" == "y" ]; then
   rm -rf _*
@@ -11,3 +20,6 @@ echo "Moving files..."
 mv html/* .
 echo "Removing old html directory"
 rm -rf html
+rm -rf tmpdocs
+rm -rf docs
+
